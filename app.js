c1483e1d951535e69cb7662a0b64a6c8c860e812
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-02-15 20:35:00 +0700

    :sparkles: use http request instead of web scrapping

--- app.js
+++ app.js
@@ -1,81 +1,48 @@
-const puppeteer = require('puppeteer')
 const express = require('express')
-const bodyParser = require('body-parser')
-const ddos = require('ddos')
-
-const screenshot = 'debug.png';
+const request = require('request');
 
 const app = express();
-const Ddos = new ddos({
-    burst: 30,
-    limit: 30,
-    maxCount: 80,
-    checkInterval: 5,
-    errormessage: "You have been blocked by attemped Too many requests"
-})
-app.use(Ddos.express);
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 
-var pm25Val = 0;
-var avgPm25Val = 0;
-var pm10Val = 0;
-var avgPm10Val = 0;
-var co2Val = 0;
+var data = {
+    lastUpdate: 0,
+    temp: 0,
+    humid: 0,
+    pm25: 0,
+    pm25Avg: 0,
+    pm10: 0,
+    pm10Avg: 0,
+    co2: 0
+}
 
-(async () => {
-  const browser = await puppeteer.launch({headless: true})
-  const page = await browser.newPage()
-  await page.goto('http://www.emtrontech.com/iaqnode/login.php')
-  await page.type('#tuser', "admin")
-  await page.type('#tpass', "12345678")
-  await page.$eval('#loginBtn', elem => elem.click())
-  await page.waitForNavigation()
-  await page.goto('http://www.emtrontech.com/iaqnode/siteData.php?id=24&type=4&sName=รุ่งอรุณ')
-  setInterval(() => {
-        var pm25 = page.$eval('#Rpm25Device', data => data.innerText);
-        var pm25Avg = page.$eval('#pm25Device', data => data.innerText);
-        var pm10 = page.$eval('#Rpm10Device', data => data.innerText);
-        var pm10Avg = page.$eval('#pm10Device', data => data.innerText);
-        var co2 = page.$eval('#Rco2Device', data => data.innerText);
-        
-        pm25.then((result)=> {
-            return pm25Val = result;
-        });
-        pm25Avg.then((result)=> {
-            return avgPm25Val = result;
-        });
-        pm10.then((result)=> {
-            return pm10Val = result;
-        });
-        pm10Avg.then((result)=> {
-            return avgPm10Val = result;
-        });
-        co2.then((result)=> {
-            return co2Val = result;
-        });
-        
-        
-    }, 3000);
-  await page.screenshot({ path: screenshot })
-  console.log('logged in')
-})()
+const options = {
+    url: 'http://www.emtrontech.com/iaqnode/getSpecSiteData.php',
+    json: true,
+    form: {
+        site: 24,
+        siteType: 4
+    }
+};
+setInterval(()=>{
+    request.post(options, (err, res, body) => {
+        if (err) {
+            return console.log(err);
+        }
+        data.lastUpdate = body.d.lastUpdate
+        data.temp = body.d.tempDevice
+        data.humid = body.d.RhumidDevice
+        data.pm25 = body.d.Rpm25Device
+        data.pm25Avg = body.d.pm25Device
+        data.pm10 = body.d.Rpm10Device
+        data.pm10Avg = body.d.pm10Device
+        data.co2 = body.d.Rco2Device
+    });    
+}, 3000)
 
 app.get('/', (req, res) => {
-        res.status(200).json({
-            success: true,
-            current_pm25: pm25Val,
-            average_pm25: avgPm25Val,
-            current_pm10: pm10Val,
-            average_pm10: avgPm10Val,
-            carbon_dioxide: co2Val
-        })
-})
-
-app.get("*", (req, res) => {
-    res.status(404).json({"error": "not found"});
-    res.end();
+    res.status(200).json({
+        success: true,
+        data: data
+    })
 })
 
 app.listen(3000, () => {
